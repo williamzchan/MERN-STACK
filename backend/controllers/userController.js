72944f16ca97cd0f87ff44c15a4853aeffaec573
William Chan <willchan@bu.edu>
2025-01-16 16:08:51 -0500

    updates to backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,13 +5,25 @@ const jwt = require('jsonwebtoken')
 const createToken = (_id) => {
     //can add token as second parameter to sign function 
     // can be any string but this is unsafe so put it into .env
-    jwt.sign({_id}, process.env.PASSWORD, {expiresIn: '3d'})
+    return jwt.sign({_id}, process.env.PASSWORD, {expiresIn: '3d'})
 }
 
 //controller for users login
 const loginUser = async (req, res) =>{
 
     const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create a user token
+        const token = createToken(user._id)
+
+        //replace user param with token, qucker authentication
+        res.status(200).json({email, token})
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
     
     res.json({mssg: 'login user'})
 
@@ -22,11 +34,14 @@ const signupUser = async(req, res) =>{
 
     const {email, password} = req.body 
 
-    // create a user token
-    const token = createToken(user._id)
+  
 
     try {
         const user = await User.signup(email, password)
+
+        // create a user token
+        const token = createToken(user._id)
+
         //replace user param with token, qucker authentication
         res.status(200).json({email, token})
     }catch(error){
